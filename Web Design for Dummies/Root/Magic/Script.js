de667d5985e3a9791e307869bc9dcbed7c05a34c
jonimenal <166454146+jonimenal@users.noreply.github.com>
2024-04-09 12:29:34 +0100

    Add files via upload

---
+++ Web Design for Dummies/Root/Magic/Script.js
@@ -0,0 +1,128 @@
+        
+        function btnOverOne(){
+            document.getElementById("btn1").style.backgroundColor = "#F98B88"
+            document.getElementById("btn1").style.color = "#000000"
+        }
+        function btnOutOne(){
+            document.getElementById("btn1").style.backgroundColor = "rgb(26, 26, 26)"
+            document.getElementById("btn1").style.color = "rgb(255, 240, 240)"
+        }
+        function btnOverTwo(){
+            document.getElementById("btn2").style.backgroundColor = "#F98B88"
+            document.getElementById("btn2").style.color = "#000000"
+        }
+        function btnOutTwo(){
+            document.getElementById("btn2").style.backgroundColor = "rgb(26, 26, 26)"
+            document.getElementById("btn2").style.color = "rgb(255, 240, 240)"
+        }
+        function btnOverThree(){
+            document.getElementById("btn3").style.backgroundColor = "#F98B88"
+            document.getElementById("btn3").style.color = "#000000"
+        }
+        function btnOutThree(){
+            document.getElementById("btn3").style.backgroundColor = "rgb(26, 26, 26)"
+            document.getElementById("btn3").style.color = "rgb(255, 240, 240)"
+        }
+        function btnOverFour(){
+            document.getElementById("btn4").style.backgroundColor = "#F98B88"
+            document.getElementById("btn4").style.color = "#000000"
+        }
+        function btnOutFour(){
+            document.getElementById("btn4").style.backgroundColor = "rgb(26, 26, 26)"
+            document.getElementById("btn4").style.color = "rgb(255, 240, 240)"
+        }
+        function btnOverFive(){
+            document.getElementById("btn5").style.backgroundColor = "#F98B88"
+            document.getElementById("btn5").style.color = "#000000"
+        }
+        function btnOutFive(){
+            document.getElementById("btn5").style.backgroundColor = "rgb(26, 26, 26)"
+            document.getElementById("btn5").style.color = "rgb(255, 240, 240)"
+        }
+        function titleOver(){
+            document.getElementById("sabo").style.color = "#404040"
+        }                
+        function titleOut(){
+            document.getElementById("sabo").style.color = "#F98B88"
+        }
+
+        function oneOver(){
+            document.getElementById("one").style.color = "#404040"
+        }
+                
+        function oneOut(){
+            document.getElementById("one").style.color = "rgb(255, 240, 240)"
+        }
+
+        function twoOver(){
+            document.getElementById("two").style.color = "#404040"
+        }
+                
+        function twoOut(){
+            document.getElementById("two").style.color = "rgb(255, 240, 240)"
+        }
+
+        function threeOver(){
+            document.getElementById("three").style.color = "#404040"
+        }
+                
+        function threeOut(){
+            document.getElementById("three").style.color = "rgb(255, 240, 240)"
+        }
+
+        function popOne(){
+            window.open("Root/SeletorPorTag.html","fenetre","wdith=600,height=600")
+        }
+
+        function popTwo(){
+            window.open("Root/SeletorPorClasse.html","fenetre","wdith=600,height=600")
+        }
+
+        function popThree(){
+            window.open("Root/SeletorPorId.html","fenetre","wdith=600,height=600")
+        }
+
+        function fourOver(){
+            document.getElementById("four").style.color = "#404040"
+        }
+                
+        function fourOut(){
+            document.getElementById("four").style.color = "rgb(255, 240, 240)"
+        }
+
+
+        function fiveOver(){
+            document.getElementById("five").style.color = "#404040"
+        }
+                
+        function fiveOut(){
+            document.getElementById("five").style.color = "rgb(255, 240, 240)"
+        }
+
+        function sixOver(){
+            document.getElementById("six").style.color = "#404040"
+        }
+                
+        function sixOut(){
+            document.getElementById("six").style.color = "rgb(255, 240, 240)"
+        }
+
+        function popFour(){
+            window.open("Root/InternalCss.html","fenetre","wdith=600,height=600")
+        }
+
+        function popFive(){
+            window.open("Root/ExternalCss.html","fenetre","wdith=600,height=600")
+        }
+
+        function popSix(){
+            window.open("Root/InlineCss.html","fenetre","wdith=600,height=600")
+        }
+
+        function inicioOver(){
+            document.getElementById("inicio").style.color = "rgb(211, 185, 185)"
+        }
+
+        function inicioOut(){
+            document.getElementById("inicio").style.color = "rgba(255, 240, 240, 0.144)"
+        }
